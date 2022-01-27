ef3f62a1c393b4e11fae8b6a90802885923a1e20
talhanuhuymaz <41086768+talhanuhuymaz@users.noreply.github.com>
2022-01-27 18:34:35 +0300

    Add files via upload

---
+++ project/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? name; 
+string? surname;
+
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+
+Console.Write("What is your surname?");
+surname = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surname);
