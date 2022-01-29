cf490386bc256fdb6ac4b54439ebc925913d8337
talhanuhuymaz <talha_nuh@hotmail.com>
2022-01-30 00:03:22 +0300

    Added Age Question

--- project/Program.cs
+++ project/Program.cs
@@ -1,11 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Console.WriteLine("What is your name?");
+// var name = Console.ReadLine();
+
+// Console.WriteLine("What is your surname?");
+// var surname = Console.ReadLine();
+
+// Console.WriteLine("Merhaba " + name + " " + surname);
 
 
-Console.WriteLine("What is your name?");
-var name = Console.ReadLine();
+var a = "Yasin kac";
 
-Console.WriteLine("What is your surname?");
-var surname = Console.ReadLine();
+Console.WriteLine(a);
 
-Console.WriteLine("Merhaba " + name + " " + surname);
+var yas = Console.ReadLine();
+
+var son = Convert.ToInt32(yas);
+
+if (son >= 18 && son !=0 )
+{
+Console.WriteLine("Partiye girebilirsin:)");
+}
+
+if (son < 18)
+{
+Console.WriteLine("Maalesef yasiniz tutmuyor:(");
+}
+
