70edaec167502d17a1e177867b0c6acc0363cfe6
talhanuhuymaz <talha_nuh@hotmail.com>
2022-01-31 00:00:03 +0300

    new parts

--- project/Program.cs
+++ project/Program.cs
@@ -36,20 +36,47 @@
 
 // PART3
 
-Console.WriteLine("how many times have you had covid ? ");
-var number = Console.ReadLine();
+// Console.WriteLine("how many times have you had covid ? ");
+// var number = Console.ReadLine();
 
-var intnumber = Convert.ToInt32(number);
+// var intnumber = Convert.ToInt32(number);
 
-if ( intnumber<3 && intnumber!=0)
+// if ( intnumber<3 && intnumber!=0)
+// {
+//     Console.WriteLine("How do you feel ? ");
+//     var feel = Console.ReadLine();
+//     Console.WriteLine("Great! ");
+// }
+// else
+// {
+// Console.WriteLine("You have to go to the hospital! ");
+// }
+
+
+// PART4 Verilen aralıkta rastgele sayı seçme
+
+// Random rnd = new Random();
+
+// int number = rnd.Next(5,10);
+
+// Console.WriteLine(number);
+
+
+
+// PART5 Basit switch/case yapısı
+
+string id = "turkey";
+switch (id)
 {
-    Console.WriteLine("How do you feel ? ");
-    var feel = Console.ReadLine();
-    Console.WriteLine("Great! ");
-}
-else
-{
-Console.WriteLine("You have to go to the hospital! ");
+    case "turkey":
+        Console.WriteLine("You can not pass");
+        break;
+    case "usa":
+        Console.WriteLine("Ok, you can pass");
+        break;
+    default:
+        Console.WriteLine("Not exist");
+        break;
 }
 
 
